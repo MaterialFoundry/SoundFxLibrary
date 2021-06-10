415373bf08104ac3e1cbde0fd4967ad3c7e0094b
CDeenen <cdeenen@outlook.com>
2021-06-10 02:17:37 +0200

    v1.0.1

--- changelog.md
+++ changelog.md
@@ -1,4 +1,17 @@
 # Changelog Sound Fx Library
 
+### v1.0.1 - 09-06-2021
+<b>File name changes</b>
+<ul>
+<li>In 'Nature/Loops/Forest Day': forest-# => forest-day-#</li>
+<li>In 'Misc/Single/Squeaky Door Open Slow': squeakydoor-open-veryslow-# => squeaky-door-open-slow-#</li>
+</ul>
+
+<b>Other</b>
+<ul>
+<li>Added pack registration for 'Soundboard by Blitz' and 'Moulinette Forge Sounds'. Module needs to be enabled for this to function.</li>
+<li>Changed folder structure: All similar sounds are now placed in a folder so 'Soundboard by Blitz' automatically loads them as wildcard sounds</li>
+</ul>
+
 ### v1.0.0 - 08-06-2021
 Initial release
--- packRegistry.js
+++ packRegistry.js
@@ -1,11 +1,12 @@
 
 const moduleName = 'soundfxlibrary',
-    soundPacks = ['Combat', 'Creatures', 'Misc', 'Nature', 'Tavern', 'Town'],
+    soundPacksSounboard = ['Combat', 'Creatures', 'Misc', 'Nature', 'Tavern', 'Town'],
+    soundPacksMoulinette = ['Combat/Loops', 'Combat/Single', 'Creatures/Animals', 'Creatures/Monsters', 'Misc/Loops', 'Misc/Single', 'Nature/Loops', 'Nature/Single', 'Tavern/Loops', 'Tavern/Single', 'Town/Loops', 'Town/Single'],
     moduleLink = 'https://github.com/CDeenen/SoundFxLibrary';
 
 /* Soundboard by Blitz */
 Hooks.once('SBPackageManagerReady', () => {
-    soundPacks.forEach((pack) => {
+    soundPacksSounboard.forEach((pack) => {
         SoundBoard.packageManager.addSoundPack(game.i18n.localize(`SOUNDFXLIBRARY.${pack}.title`), `modules/${moduleName}/${pack}`, moduleName, {
             description: game.i18n.localize(`SOUNDFXLIBRARY.${pack}.description`),
             licenses: [{
@@ -23,7 +24,7 @@ Hooks.once('SBPackageManagerReady', () => {
 /* Moulinette */
 Hooks.once('ready', () => {
     if (game.moulinette) {
-        const moulinetteSources = soundPacks.map((pack) => ({
+        const moulinetteSources = soundPacksMoulinette.map((pack) => ({
             type: 'sounds',
             publisher: 'CDeenen',
             pack,
